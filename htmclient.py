feca17e62e954c8f070a7bff7cbf20e9fd07676a
Matthew Taylor <matt@numenta.org>
2015-12-09 20:18:32 -0800

    Updated htm http url

--- htmclient.py
+++ htmclient.py
@@ -1,7 +1,8 @@
 import json
 import requests
 
-URL = "http://localhost:5000/"
+# URL = "http://localhost:5000/"
+URL = "https://htm-over-http.herokuapp.com/"
 
 def post(url, params=None):
     return requests.post(URL+url, data=params)
