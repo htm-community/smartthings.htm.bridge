3639545ae91cc997c4c32167bcb5d31af166197c
Jeff Fohl <jeff@fohl.com>
2015-12-17 17:24:33 -0800

    Basic directive functionality in place.

--- charts/src/js/directives/stbChart.js
+++ charts/src/js/directives/stbChart.js
@@ -6,27 +6,69 @@ angular.module('app').directive('stbChart', ['$http', 'stbUtils', function($http
       sensorSince : "@",
       maxRows : "@"
     },
+    replace: true,
+    template: "<div class='chart'></div>",
     link: function(scope, element, attrs) {
 
       scope.view = {
-        graph : null
+        chart : null
+      };
+
+      var updateMinMaxDates = function() {
+
+      };
+
+      var removeStringData = function(data) {
+        var stringColumns = ['component', 'timezone'];
+        var doomedIndexes = [];
+        var series = data.series[0];
+        angular.forEach(series.columns, function(name, index) {
+            if (stringColumns.indexOf(name) !== -1) {
+                doomedIndexes.push(index);
+            }
+        });
+        // Now that we know which indexes are doomed, we reverse the order so we
+        // can extract them from the list from the end
+        doomedIndexes = doomedIndexes.reverse();
+        angular.forEach(doomedIndexes, function(doomed) {
+            series.columns.splice(doomed, 1);
+        });
+        angular.forEach(series.values, function(row) {
+            angular.forEach(doomedIndexes, function(doomed) {
+                row.splice(doomed, 1);
+            });
+        });
+      };
+
+      var convertJsonDataToCsv = function(data) {
+        var rows = [];
+        var series = data.series[0];
+
+        var headers = series.columns;
+        var values = series.values;
+
+        rows.push(headers);
+
+        _.each(values, function(values) {
+            rows.push(values.join(','));
+        });
+
+        return rows.join('\n');
       };
 
       // load the data
-      var getData = function(sensorName) {
-        console.log(scope.sensorName);
+      var getData = function() {
         var dataUrl = '/_data/sensor/' + scope.sensorName;//  + '?limit=' + scope.maxRows;
         /*
         if (sensorSince) {
             dataUrl += '&since=' + sensorSince;
         }
         */
-        //var id = scope.sensorName.replace('/', '_').replace(/\+/g, '-');
         $http.get(dataUrl).then(function(sensorData) {
-          console.log(sensorData);
+          // console.log(sensorData);
+          scope.view.chart = renderChart(sensorData.data);
           /*
-          var chart = renderSensorChart(id, sensorData);
-          updateMinMaxDates(chart);
+          updateMinMaxDates(scope.view.chart);
           charts.push(chart);
           if (charts.length == sensors.length) {
             _.each(charts, function(chart) {
@@ -48,10 +90,7 @@ angular.module('app').directive('stbChart', ['$http', 'stbUtils', function($http
       var renderChart = function(data) {
         removeStringData(data);
         var csvString = convertJsonDataToCsv(data);
-        // var width = $('#' + id + '-container').width();
-        return new Dygraph(element, csvString, {
-            // width: width,
-            height: 400,
+        return new Dygraph(element[0], csvString, {
             series: {
               value: {
                 strokeWidth: 2,
