2f5cd43980e1b83398e2d0b3618f1ab6cb8f2d57
Jeff Fohl <jeff@fohl.com>
2015-12-18 08:43:44 -0800

    Working graph directive, but needs more work.

--- charts/src/js/directives/stbChart.js
+++ charts/src/js/directives/stbChart.js
@@ -14,10 +14,20 @@ angular.module('app').directive('stbChart', ['$http', 'stbUtils', function($http
         chart : null
       };
 
-      var updateMinMaxDates = function() {
-
+      var i;
+      /*
+      var updateMinMaxDates = function(chart) {
+        var extremes = chart.xAxisExtremes();
+        var min = extremes.shift();
+        var max = extremes.shift();
+        if (! minDate || min < minDate) {
+            minDate = min;
+        }
+        if (! maxDate || max > maxDate) {
+            maxDate = max;
+        }
       };
-
+      */
       var removeStringData = function(data) {
         var stringColumns = ['component', 'timezone'];
         var doomedIndexes = [];
@@ -40,20 +50,69 @@ angular.module('app').directive('stbChart', ['$http', 'stbUtils', function($http
         });
       };
 
-      var convertJsonDataToCsv = function(data) {
-        var rows = [];
-        var series = data.series[0];
+      // parseDate():
+      // takes a string and attempts to convert it into a Date object
+      // return: Date object, or null if parsing failed
+      var parseDate = function(strDateTime) { // FIXME: Can using the ISO format simplify this?
+        // can we get the browser to parse this successfully?
+        var numDate = new Date(strDateTime);
+        if (numDate.toString() !== "Invalid Date") {
+          return numDate;
+        }
+        var dateTime = String(strDateTime).split(" "); // we are assuming that the delimiter between date and time is a space
+        var args = [];
+        // is the date formatted with slashes or dashes?
+        var slashDate = dateTime[0].split("/");
+        var dashDate = dateTime[0].split("-");
+        if ((slashDate.length === 1 && dashDate.length === 1) || (slashDate.length > 1 && dashDate.length > 1)) {
+          // if there were no instances of delimiters, or we have both delimiters when we should only have one
+          handleError("Could not parse the timestamp", "warning", true);
+          return null;
+        }
+        // if it is a dash date, it is probably in this format: yyyy:mm:dd
+        if (dashDate.length > 2) {
+          args.push(dashDate[0]);
+          args.push(dashDate[1]);
+          args.push(dashDate[2]);
+        }
+        // if it is a slash date, it is probably in this format: mm/dd/yy
+        else if (slashDate.length > 2) {
+          args.push(slashDate[2]);
+          args.push(slashDate[0]);
+          args.push(slashDate[1]);
+        } else {
+          handleError("There was something wrong with the date in the timestamp field.", "warning", true);
+          return null;
+        }
+        // is there a time element?
+        if (dateTime[1]) {
+          var time = dateTime[1].split(":");
+          args = args.concat(time);
+        }
+        for (var t = 0; t < args.length; t++) {
+          args[t] = parseInt(args[t]);
+        }
+        numDate = new Function.prototype.bind.apply(Date, [null].concat(args));
+        if (numDate.toString() === "Invalid Date") {
+          handleError("The timestamp appears to be invalid.", "warning", true);
+          return null;
+        }
+        return numDate;
+      };
 
-        var headers = series.columns;
-        var values = series.values;
-
-        rows.push(headers);
-
-        _.each(values, function(values) {
-            rows.push(values.join(','));
-        });
-
-        return rows.join('\n');
+      var setDates = function(data) {
+        // find timestamp column
+        var index = 0;
+        for (i = 0; i < data.series[0].columns.length; i++) {
+          if (data.series[0].columns[i] === "time" || data.series[0].columns[i] === "timestamp") {
+            index = i;
+            break;
+          }
+        }
+        for (i = 0; i < data.series[0].values.length; i++) {
+          data.series[0].values[i][index] = parseDate(data.series[0].values[i][index]);
+        }
+        return data;
       };
 
       // load the data
@@ -65,8 +124,10 @@ angular.module('app').directive('stbChart', ['$http', 'stbUtils', function($http
         }
         */
         $http.get(dataUrl).then(function(sensorData) {
-          // console.log(sensorData);
+          removeStringData(sensorData.data);
+          setDates(sensorData.data);
           scope.view.chart = renderChart(sensorData.data);
+          //updateMinMaxDates(scope.view.chart);
           /*
           updateMinMaxDates(scope.view.chart);
           charts.push(chart);
@@ -88,9 +149,11 @@ angular.module('app').directive('stbChart', ['$http', 'stbUtils', function($http
       // render the graph
 
       var renderChart = function(data) {
-        removeStringData(data);
-        var csvString = convertJsonDataToCsv(data);
-        return new Dygraph(element[0], csvString, {
+        return new Dygraph(
+          element[0],
+          data.series[0].values,
+          {
+            labels: data.series[0].columns,
             series: {
               value: {
                 strokeWidth: 2,
