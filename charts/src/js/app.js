4fc7d894a4019425b8f4c003caa9c4e3333fc31f
Jeff Fohl <jeff@fohl.com>
2015-12-19 14:10:06 -0800

    Set some default config values, and fixed #17.

--- charts/src/js/app.js
+++ charts/src/js/app.js
@@ -1,2 +1,8 @@
 angular.module('app', ['ui.bootstrap']);
 
+// some Settings:
+angular.module('app').constant('CONFIG', {
+  'STRING_COLUMNS' : ['component', 'timezone'],
+  'DEFAULT_LIMIT' : 100
+});
+
